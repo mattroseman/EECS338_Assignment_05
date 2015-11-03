1fe37a30b63d3fa65a47e4e006fae8012794bfbe
mroseman95 <mrr77@case.edu>
2015-11-03 00:59:45 -0500

    mad basic skeleton, and initialized values
    Signed-off-by: mroseman95 <mrr77@case.edu>

--- Hw5.c
+++ Hw5.c
@@ -7,6 +7,9 @@
 #define WTR 2
 #define RDR 3
 
+void Writer();
+void Reader();
+
 /* Semaphores Initial Values */
 unsigned int InitVal[] = {1,0,0,0};
 
@@ -18,7 +21,73 @@ unsigned int nreaders;
 unsigned int Busy;
 unsigned int RBlocked;
 
-int main()
+int semid;
+
+unsigned int numThreads;
+
+unsigned int sleepScale;
+
+int main(int argc, char *argv[])
+{
+    sleepScale = 1;
+
+    if (argc > 1)
+    {
+        if (sscanf(argv[1], "%d", &numThreads) < 0)
+        {
+            perror("sscanf failed\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        numThreads = 10;
+    }
+
+/* Initialize Semaphores */
+    semid = CreateGroup(KEY, NUM_SEM, InitVal);
+
+/* Initialize Variables */
+    nwriters = 0;
+    nreaders = 0;
+    Busy = 0;
+    RBlocked = 0;
+
+    int i;
+    unsigned int seed;
+    if ((seed = time(NULL)) < 0)
+    {
+        perror("time failed\n");
+        exit(EXIT_FAILURE):
+    }
+    if (srand(seed) < 0)
+    {
+        perror("srand failed\n");
+        exit(EXIT_FAILURE):
+    }
+    for (i = 0; i < numThreads, i++)
+    {
+        unsigned int process = rand()%2;
+        sleep(2 * sleepScale);
+        if (process == 0)
+        {
+            // start read
+        }
+        if (process == 1)
+        {
+            // start write
+        }
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
+void Reader()
+{
+
+}
+
+void Writer()
 {
 
 }
