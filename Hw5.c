a6ba9b22cf93b246ce083ce1e12d01c2569dc106
mroseman95 <mrr77@case.edu>
2015-11-06 15:18:13 -0500

    added a signature to all output
    Signed-off-by: mroseman95 <mrr77@case.edu>

--- Hw5.c
+++ Hw5.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <semaphore.h>
 #include <pthread.h>
+#include <sys/types.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -16,6 +17,11 @@ sem_t mutex, rmutex, rdr, wtr;
 
 int semid;
 
+/* Threads */
+int rc;
+
+pthread_attr_t attr;
+
 /* Integers */
 unsigned int nwriters;
 unsigned int nreaders;
@@ -26,11 +32,21 @@ unsigned int RBlocked;
 
 unsigned int numThreads;
 
+/* Misc. */
 unsigned int sleepScale;
+
+char * signature;
 
 int main(int argc, char *argv[])
 {
     sleepScale = 1;
+
+    signature = malloc(50 * sizeof(char));
+    if (sprintf(signature, "---TID %d: ", gettid()) < 0)
+    {
+        perror ("sprintf failed\n");
+        exit(EXIT_FAILURE);
+    }
 
     if (argc > 1)
     {
@@ -52,7 +68,7 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    printf("The semaphore mutex has been initialized with value 1\n");
+    printf("%sThe semaphore mutex has been initialized with value 1\n", signature);
 
     if (sem_init(&rmutex, 0, (unsigned int)0) < 0)
     {
@@ -60,7 +76,7 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    printf("The semaphore rmutex has been initialized with value 0\n");
+    printf("%sThe semaphore rmutex has been initialized with value 0\n", signature);
 
     if (sem_init(&wtr, 0, (unsigned int)0) < 0)
     {
@@ -68,7 +84,7 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    printf("The semaphore wtr has been initialized with value 0\n");
+    printf("%sThe semaphore wtr has been initialized with value 0\n", signature);
 
     if (sem_init(&rdr, 0, (unsigned int)0) < 0)
     {
@@ -76,7 +92,7 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    printf("The semaphore rdr has been initialized with value 0\n");
+    printf("%sThe semaphore rdr has been initialized with value 0\n", signature);
 
 /* Initialize Variables */
     nwriters = 0;
@@ -93,20 +109,25 @@ int main(int argc, char *argv[])
     }
     srand(seed);
 
+    printf("%sThe random number generator has been seeded\n", signature);
+
 /* Initialize Threads */
     pthread_t *threads = malloc(numThreads * sizeof(pthread_t));
 
-    pthread_attr_t *attr;
-    if (pthread_attr_init(attr) != 0)
+    if ((rc = pthread_attr_init(&attr)) != 0)
     {
         perror("pthread_attr_init() failed\n");
+        printf("return code %d\n", rc);
         exit(EXIT_FAILURE);
     }
-    if (pthread_attr_setdetachstate(attr, PTHREAD_CREATE_JOINABLE) != 0)
+    if ((rc = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE)) != 0)
     {
         perror("pthread_attr_setdetachstate() failed\n");
+        printf("return code %d\n", rc);
         exit(EXIT_FAILURE);
     }
+
+    printf("%sThe pthread attributes have been set\n", signature);
 
 /* Start Threads */
     for (i = 0; i < numThreads; i++)
@@ -116,20 +137,24 @@ int main(int argc, char *argv[])
         if (process == 0)
         {
             // Start Reader
-            if (pthread_create(&threads[i], NULL, &Reader, NULL) != 0)
+            if ((rc = pthread_create(&threads[i], NULL, &Reader, NULL)) != 0)
             {
                 perror("pthread_create() for Reader failed\n");
+                printf("return code %d\n", rc);
                 exit(EXIT_FAILURE);
             }
+            printf("%sStarting Reader\n", signature);
         }
         if (process == 1)
         {
             // Start Writer
-            if (pthread_create(&threads[i], NULL, &Writer, NULL) != 0)
+            if ((rc = pthread_create(&threads[i], NULL, &Writer, NULL)) != 0)
             {
                 perror("pthread_create() for Writer failed\n");
+                printf("return code %d\n", rc);
                 exit(EXIT_FAILURE);
             }
+            printf("%sStarting Writer\n", signature);
         }
     }
 
@@ -137,18 +162,20 @@ int main(int argc, char *argv[])
     // Wait for all the threads to finish and rejoin the main thread
     for (i = 0; i < numThreads; i++)
     {
-        if(pthread_join(threads[i], NULL) != 0)
+        if((rc = pthread_join(threads[i], NULL)) != 0)
         {
             perror("pthread_join() failed\n");
+            printf("return code %d\n", rc);
             exit(EXIT_FAILURE);
         }
     }
 
     free(threads);
 
-    if (pthread_attr_destroy(attr) != 0)
+    if ((rc = pthread_attr_destroy(&attr)) != 0)
     {
         perror("pthread_attr_destroy() failed\n");
+        printf("return code %d\n", rc);
         exit(EXIT_FAILURE);
     }
 
@@ -173,11 +200,20 @@ int main(int argc, char *argv[])
         exit(EXIT_SUCCESS);
     }
 
+    free(signature);
+
     exit(EXIT_SUCCESS);
 }
 
 void *Reader(void *arg)
 {
+    char * rsignature = malloc(50 * sizeof(char));
+    if (sprintf(rsignature, "---TID %d: ",   (int)gettid()) < 0)
+    {
+        perror ("sprintf failed for rsignature\n");
+        exit(EXIT_FAILURE);
+    }
+
     sem_wait(&rmutex);
     sem_wait(&mutex);
 
@@ -203,11 +239,21 @@ void *Reader(void *arg)
     }
     sem_post(&mutex);
 
+    printf("%sReader done\n", rsignature);
+
+    free(rsignature);
     pthread_exit(NULL);
 }
 
 void *Writer(void *arg)
 {
+    char * wsignature = malloc(50 * sizeof(char));
+    if (sprintf(wsignature, "---TID %d: ",   (int)gettid()) < 0)
+    {
+        perror ("sprintf failed\n");
+        exit(EXIT_FAILURE);
+    }
+
     sem_wait(&mutex);
     nwriters++;
     if (Busy || nreaders > 0)
@@ -237,5 +283,8 @@ void *Writer(void *arg)
     }
     sem_post(&mutex);
 
+    printf("%sWriter done\n", wsignature);
+
+    free(wsignature);
     pthread_exit(NULL);
 }
